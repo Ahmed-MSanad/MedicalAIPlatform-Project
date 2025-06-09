880d0e77c2b984001a23e1d4834c38d2c13f0647
ahmed-mahmoud-goda <ahmed-goda@hotmail.com>
2025-06-09 22:24:37 +0300

    changed background

--- Backend/AiDrivenMedicalPlatform/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Backend/AiDrivenMedicalPlatform/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5cabd058b3e9ac54aedbb47bea782c1d742d0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1b25c32057b6232b1bd31f26c1d5830f12219d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
